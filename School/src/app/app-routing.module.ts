7f91464c346a1ea7d6ec47af14fc8cfa17807f3e
EinsteinElaim <einsteineliam@gmail.com>
2020-10-02 02:36:04 +0300

    added programs component

--- School/src/app/app-routing.module.ts
+++ School/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './about/about.component'
 import { AboutusComponent } from './aboutus/aboutus.component'
 import { FormComponent } from './form/form.component'
 import { HomeComponent } from './home/home.component'
+import { ProgramsComponent } from './programs/programs.component'
 
 const routes: Routes = [
   {path: 'form', component: FormComponent},
   { path:'home', component: HomeComponent},
   {path:'about', component: AboutComponent},
   {path:'aboutus', component: AboutusComponent},
+  {path:'programs', component: ProgramsComponent},
 
 ]
 
