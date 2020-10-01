7c4109839129b975056082281960effa4fb67b67
wil-bert <ianwilbuts@gmail.com>
2020-10-01 08:27:42 +0300

    add routes

--- School/src/app/app-routing.module.ts
+++ School/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
+import { AboutComponent } from './about/about.component'
+import { AboutusComponent } from './aboutus/aboutus.component'
 import { FormComponent } from './form/form.component'
+import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
-  {
-    path: 'Schools',
-    component: FormComponent,
-  },
+  {path: 'form', component: FormComponent},
+  { path:'home', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'aboutus', component: AboutusComponent},
+
 ]
 
 @NgModule({
