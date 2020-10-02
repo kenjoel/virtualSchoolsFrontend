2c53380f8a1ddb3ad3f58d650a5ab4abd2ec2d1e
wil-bert <ianwilbuts@gmail.com>
2020-10-02 08:49:39 +0300

    new changes

--- School/src/app/students/notes/notes.component.ts
+++ School/src/app/students/notes/notes.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {Notes} from '../../notes';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notes = [
+    new Notes('Ian Wilbert','Form Three','any')
+  ]
+
+  @Output () emitNotes = new EventEmitter()
+
 
   constructor() { }
 
